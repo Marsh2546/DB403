fd863ebae4adf46a6c16a88c3ac8c172ef55ec1f
Marsh2546 <phurin.smart2546@gmail.com>
2023-11-28 10:13:27 +0700

    porject

---
+++ project/frame/index_staff.php
@@ -0,0 +1,52 @@
+<?php
+  include 'header.php';
+  $sql = "SELECT * FROM activity  A
+  WHERE start >= NOW() AND available > 0
+  AND NOT EXISTS (
+    SELECT activityID
+    FROM register
+    WHERE studentID='{$_SESSION['USER']['studentID']}'
+    AND activityID=A.activityID
+      )";
+  $result = $conn->query($sql);
+?>
+<table class="table">
+  <tr>
+    <th>Activity Name</th>
+    <th>Begin</th>
+    <th>End</th>
+    <th>Available</th>
+    <th></th>
+  </tr>
+  <?php
+    while($row = $result->fetch_assoc()){
+      echo "<tr>
+            <td>{$row['activityName']}</td>
+            <td>{$row['start']}</td>
+            <td>{$row['end']}</td>
+            <td>{$row['available']}</td>
+            <td><button class='btn btn-success btn-sm' 
+            onclick='register({$row['activityID']})'>Register</button></td>
+          </tr>";
+  } 
+  ?>
+</table>
+</div>
+<div id="overlay" class="position-fixed top-0  start-0 w-100 h-100 d-none"></div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script>
+  document.getElementById('nav-activity').classList.add('active');
+  function register(id) {
+    document.querySelector('#overlay').classList.remove('d-none');
+    fetch('register.php?id='+id).then(
+      data => data.json()
+    ).then(
+      data => {
+        alert(data.status);
+        location.reload();
+      }
+    );
+  }
+</script>
+</body>
+</html>
---
+++ project/frame/signin.php
@@ -0,0 +1,114 @@
+<?php
+  session_start();
+  if (isset ($_POST['submit'])){
+    require 'connect.php';
+    $studentID = $_POST['studentID'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM (
+            SELECT studentID id,studentName name,password pass,0 status FROM student
+            UNION
+            SELECT staffID id,staffName name,password pass,1 status FROM staff
+            ) roles
+            WHERE id = '{$studentID}'";
+      try {
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        if($row){
+          if(password_verify($password, $row['pass'])) {
+            $_SESSION['USER'] = [
+              'studentID' => $row['id'],
+              'studentName' => $row['name'],
+              'staff' => $row['status']
+            ];
+            if ($row['status'] == 0) {
+                header('location:index.php');
+            }
+            else {
+                header('location:index_staff.php');
+            }
+            exit;
+          }
+          else{
+            $err = 'password not correct!';
+          }
+          }
+          else{
+            $err = 'StudentID not found!';
+        }
+      }
+      catch(Exception $e){
+        echo $e;
+      }
+      finally{
+        $conn->close();
+      }
+  }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>student activity > signup</title>    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+      html,
+      body {
+        height: 100%;
+      }
+
+      .form-signup {
+        max-width: 330px;
+        padding: 1rem;
+      }
+
+      .form-signup .form-floating:focus-within {
+        z-index: 2;
+      }
+
+      .form-signup input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .form-signup input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+      }
+    </style>
+  </head>
+  <body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main class="form-signup w-100 m-auto">
+      <form method = "post">
+      <img class="rounded mx-auto d-block position-static top-20 start-100 bottom-50" class="mb-4" src="img/apple.png" alt="" width="130" height="">
+      <br></br>
+        <!-- <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+  <?php
+    if(isset($err)){
+      echo "<div class='alert alert-danger'>$err</div>";
+    }
+    else{
+
+    }
+  ?>
+        <div class="form-floating">
+          <input name="studentID" type="text" class="form-control" id="floatingEmail" placeholder="Email address">
+          <label for="floatingEmail">Student ID</label>
+        </div>
+        <div class="form-floating">
+          <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Password</label>
+        </div>
+    
+        <button class="btn btn-primary w-100 py-2" type="submit" name = "submit" >Sign in</button>  
+        <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="signup.php" class="link-danger">Signup now</a></p>
+        <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+        <!-- <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p> -->
+      </form>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
